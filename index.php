5574bc65e2c65a45e2d7063f9e0ea941c599d870
douglasmariano <douglas.arm@live.com>
2022-01-21 19:46:44 -0300

    aula 3

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php include("config/config.php"); ?>
-<!DOCTYPE html>
+    <!doctype html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calendário Agenda PHP</title>
+    <link rel="stylesheet" href="<?php echo DIRPAGE.'lib/css/style.css'; ?>">
+    <link rel="stylesheet" href="<?php echo DIRPAGE.'lib/js/FullCalendar/main.min.css'; ?>">
 </head>
 <body>
+    <div class="calendar"></div>
 
-<?php
-    $objeto=new Classes\ClassTeste();
-?>
-    
+    <script src="<?php echo DIRPAGE.'lib/js/FullCalendar/main.min.js'; ?>"></script>
+    <script src="<?php echo DIRPAGE.'lib/js/javascript.js'; ?>"></script>
 </body>
 </html>
